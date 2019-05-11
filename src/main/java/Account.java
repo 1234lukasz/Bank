e07b1899fdd580b0ed55dd8d0200ab5b89fd58cb
Lukasz Fijol <l.fijol@gmail.com>
2019-05-11 12:27:34 +0200

    second

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,31 @@
 public abstract class Account {
 
+    String name;
+    long accountNumber;
+    double accountBalance;
+    double changeCash;
+
+    public enum accountType{PUBL, COMP}
+
+    public void open(String name, long accountNumber, double accountBalance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+    }
+
+    public void close(String name, long accountNumber, double accountBalance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+    }
+
+    public void add(long accountNumber, double accountBalance, double changeCash) {
+        accountBalance = accountBalance + changeCash;
+    }
+
+    public void subt(long accountNumber, double accountBalance, double changeCash) {
+        accountBalance = accountBalance - changeCash;
+    }
+
+
 }
